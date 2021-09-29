d156e9fc2f5e17b04438ae3a404335384bdfc337
Abel <ccabel42@gmail.com>
2021-09-29 11:36:36 +0800

    create save project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,6 +13,9 @@ class ProjectController extends Controller
     {
         if (Auth::id()) {
             $projectData = $request->project;
+            $companyData = $request->project->company;
+            $company = Company::updateOrCreate(['name' => $companyData['name']], $companyData);
+            $projectData->company_id = $company->id;
             $project = Project::create($projectData);
             return response()->json([
                 'success' => true,
