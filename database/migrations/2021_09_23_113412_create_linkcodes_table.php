fed98544d3a440557011e169c1501c980758de8f
Abel <ccabel42@gmail.com>
2021-10-19 09:56:45 +0800

    modify qa issue

--- database/migrations/2021_09_23_113412_create_linkcodes_table.php
+++ database/migrations/2021_09_23_113412_create_linkcodes_table.php
@@ -17,7 +17,7 @@ class CreateLinkcodesTable extends Migration
             $table->id();
             $table->timestamps();
             $table->string('code')->nullable()->comment('連結碼');
-            $table->string('name')->comment('問卷類型');
+            $table->string('name')->nullable()->comment('問卷類型');
             $table->integer('count')->default(1)->comment('問卷份數');
             $table->unsignedBigInteger('project_id')->comment('專案');
             $table->foreign('project_id')
